cf8cb6d291c6a0d69fd970fb2d508d005da54873
Lee Lacanlale <lee.lacanlale@gmail.com>
2019-10-24 22:51:52 -0700

    Updates

---
+++ precache-manifest.7a84e6b51a0e2d7e60cbfd6fdc3532fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce3eee3456b96f245d16cf105c1ac4a",
+    "url": "/robofinder/index.html"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofinder/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "193e6605f698e409f85e",
+    "url": "/robofinder/static/css/main.c463da9b.chunk.css"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofinder/static/js/2.2a21f297.chunk.js"
+  },
+  {
+    "revision": "193e6605f698e409f85e",
+    "url": "/robofinder/static/js/main.79aec6b4.chunk.js"
+  },
+  {
+    "revision": "1ee620f93b1df65ad042",
+    "url": "/robofinder/static/js/runtime-main.bbe273ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofinder/static/media/SEGA.12a3db8b.woff"
+  }
+]);
